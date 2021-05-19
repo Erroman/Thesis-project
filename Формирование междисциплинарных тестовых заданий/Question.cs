319b9b48d934eab456c69cbde29de7cf3ea5c395
Vladimir Romanovskiy <vladimir_roman@mail.ru>
2021-05-19 20:17:53 +0500

    В код программы добавлены 2 класса : для предметов и  для вопросов по предметам

---
+++ Формирование междисциплинарных тестовых заданий/Question.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Формирование_междисциплинарных_тестовых_заданий
+{
+    public class Question
+    {
+        public int QuestionId { get; set; }
+        public string QuestionText { get; set; }
+
+        public int SubjectId { get; set; }
+        public virtual Subject Subject { get; set; }
+    }
+}
---
+++ Формирование междисциплинарных тестовых заданий/Subject.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections.Generic;
+using System.Collections.ObjectModel;
+
+namespace Формирование_междисциплинарных_тестовых_заданий
+{
+    public class Subject
+    {
+        public int SubjectId { get; set; }
+        public string SubjectName { get; set; }
+
+        public virtual ICollection<Question>
+            Questions
+        { get; private set; } =
+            new ObservableCollection<Question>();
+    }
+}
