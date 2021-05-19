e3b20482f087de7bd2dd49adcd66a2567e3e6c22
Vladimir Romanovskiy <vladimir_roman@mail.ru>
2021-05-19 21:18:20 +0500

    Добавлен класс для управления базой данных QuestionContext

---
+++ Формирование междисциплинарных тестовых заданий/QuestionContext.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Формирование_междисциплинарных_тестовых_заданий
+{
+    class QuestionContext
+    {
+    }
+}
